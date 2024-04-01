604763fd528a5d30a6e834f30355da3f3a8f3cda
IvanRulevskyi <rulevskiy4@gmail.com>
2024-04-01 23:04:20 +0300

    Initial Commit

---
+++ GitHub/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GitHub
+//
+//  Created by Иван Рулевский on 01.04.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
